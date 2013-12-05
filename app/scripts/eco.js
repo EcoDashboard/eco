e86563dd1ad3dbcaa7f5ff1523468c9142651d7a
Daniel Alan Miller <dalan.miller@gmail.com>
2013-12-05 20:56:29 +1030

    Fully integrated into AngularJS framework

---
+++ app/scripts/eco.js
@@ -0,0 +1,60 @@
+
+var app = angular.module('eco', [ 'ngRoute' ]);
+ 
+// app.config(['$compileProvider', function ($compileProvider){
+//   // Needed for routing to work
+//   $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+// }]);
+
+// app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+//   $routeProvider.when('/', { 
+//     templateUrl: 'index.html',
+//     controller: 'homeCtrl'
+//   });
+
+//   $routeProvider.otherwise({
+//     redirectTo: '/'
+//   });
+
+// }]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeCtrl'
+      }).
+      when('/city/:cityId', {
+        templateUrl: 'city-profile.html',
+        controller: 'cityProfileCtrl'
+      }).
+      when('/about_us', {
+        templateUrl: 'aboutus.html',
+        controller: 'aboutUsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+
+app.controller('homeCtrl', ['$scope', function($scope){
+
+	$('nav').removeClass("navbar-fixed-top").addClass("navbar-fixed-bottom");
+
+}]);
+
+
+app.controller('cityProfileCtrl', ['$scope', function($scope){
+
+	console.log("Wat wat");
+
+}]);
+
+app.controller('aboutUsCtrl', ['$scope', function($scope){
+
+	console.log("Wat wat");
+
+}]);
