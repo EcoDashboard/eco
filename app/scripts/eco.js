f1a3dc7c39802f7e91b9e1da81b40e7b6ea7380e
Daniel Alan Miller <dalan.miller@gmail.com>
2013-12-13 13:57:33 +1030

    All the changes

--- app/scripts/eco.js
+++ app/scripts/eco.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('eco', [ 'ngRoute' ]);
+var app = angular.module('eco', [ 'ngRoute' ,'tc.chartjs' ]);
  
 // app.config(['$compileProvider', function ($compileProvider){
 //   // Needed for routing to work
@@ -60,7 +60,14 @@ app.factory('City', ['$http', function($http){
 				"country_name"   : "United States",
 				"email"		     : "daniel@seattle.org",
 				"contact_number" : "12065558050",
-				"council_address": "1st Ave, Seattle WA 98105"
+				"council_address": "1st Ave, Seattle WA 98105",
+				"index_data" : {
+					"air_index": 65,
+					"water_index": 88,
+					"earth_index": 45,
+					"waste_index": 99,
+					"energy_index": 33
+				}
 			}
 	);
 
@@ -126,23 +133,67 @@ app.controller('cityProfileCtrl', ['City', '$scope', '$location', '$routeParams'
 	city = City.get($routeParams.cityId);
 
 	console.log("cityId "+ $routeParams.cityId);
-	console.log(city);
+
+	colors = {
+		'air'   : '#BEE7F2',
+		'water' : "#00A1EF",
+		'earth' : "#63CE0A",
+		'waste' : "#a1a1a1",
+		'energy': "#FAEE40"
+	};
+
 
 	if (city != null){
 
 		$scope.city = city;
-		// $scope.city_name = city.city_name;
-		// $scope.city_id = city.city_id;
+		
+		//ELEMENT DATA SETUP 
+		for( var d in city.index_data) {
+
+			console.log(city.index_data[d]);
+			o = [
+				{value : city.index_data[d], color : '#fff'},
+				{value : 100 - city.index_data[d], color : colors[d.split("_")[0]]}
+			]
+			$scope[d] = o;
+		}
+
+		$scope.chart_options = {
+			percentageInnerCutout : 70,
+			animationSteps : 60,
+			animationEasing : "easeOutCubic",
+			// animateScale : true,
+			segmentShowStroke: false
+		};
+
+
+   		$('.indicators div').hover(
+    	function(d){
+    		console.log(d.target);
+    		$(d.target).closest('.indicators').find('.row.collapse').collapse('show');
+    	},function(d){
+    		$(d.target).closest('.indicators').find('.row.in').collapse('hide');
+    	}); 
+
+
 
 	} else {
 
 		$location.path('/');
 
 	}
+
+	
 	
 }]);
 
 app.controller('aboutUsCtrl', ['$scope', function($scope){
+
+	console.log("Wat wat");
+
+}]);
+
+app.controller('dashboardCtrl', ['$scope', function($scope){
 
 	console.log("Wat wat");
 
